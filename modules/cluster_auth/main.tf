c5d129db139a367949b68d7f77c755ac5f535375
luqmanMohammed <m.luqman077@gmail.com>
2023-06-28 22:23:06 +0100

    Add cluster auth module

---
+++ modules/cluster_auth/main.tf
@@ -0,0 +1,36 @@
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "kubernetes_config_map" "aws_auth_cm" {
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+}
+
+locals {
+  base_map_roles = yamldecode(try(data.kubernetes_config_map.aws_auth_cm.data["mapRoles"], "---"))
+  base_map_users = yamldecode(try(data.kubernetes_config_map.aws_auth_cm.data["mapUsers"], "---"))
+
+  combined_map_roles = concat(local.base_map_roles, var.auth_config_map_roles)
+  combined_map_users = concat(local.base_map_users, var.auth_config_map_users)
+}
+
+import {
+  to = kubernetes_config_map.aws_auth_cm
+  id = "kube-system/aws-auth"
+}
+
+
+resource "kubernetes_config_map" "aws_auth_cm" {
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode(local.combined_mapRoles)
+    mapUseres = yamlencode(local.mapUsers)
+  }
+}
---
+++ modules/cluster_auth/variables.tf
@@ -0,0 +1,19 @@
+variable "auth_config_map_roles" {
+  type = list(object({
+    groups = set(list)
+    rolearn = string
+    username = optional(string, "{{SessionName}}")
+  }))
+}
+
+variable "auth_config_map_users" {
+  type = list(object({
+    groups = set(list)
+    rolearn = string
+    username = optional(string, "{{SessionName}}")
+  }))
+}
+
+variable "cluster_name" {
+  type = string
+}
